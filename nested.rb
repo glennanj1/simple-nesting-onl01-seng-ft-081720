369a940cbdfc7aa96d1d9264d5465f71de2b0249
John Glennan <glennan20@gmail.com>
2020-08-26 18:11:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,8 @@ def adding_matz
 
       programmer_hash[:yukihiro_mtsumoto] = {
         known_for: "Ruby"
-        languages: ["LISP", "C"]
+        languages: 
+        ["LISP", "C"]
         
       }
         
